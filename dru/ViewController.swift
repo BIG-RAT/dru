049bdfd658c7eeb22c46ce1d92f09f55c20ad13b
Leslie Helou <leslie@ccs-indy.com>
2017-08-07 13:55:05 -0400

    Initial Commit

---
+++ dru/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  dru
+//
+//  Created by Leslie Helou on 8/7/17.
+//  Copyright © 2017 jamf. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
